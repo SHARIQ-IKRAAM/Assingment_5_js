c59714d30ffa2cc6122b3a3fd789d9756e5066f8
SHARIQ-IKRAAM <144127019+SHARIQ-IKRAAM@users.noreply.github.com>
2023-09-25 18:02:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,146 @@
+                     // JS ASSIGNMENT #5
+           //   Chapter 20
+
+  // 1.)
+    var allLower = userInput.toLowerCase();
+ // 2.)
+  x = x.toLowerCase();
+ // 3.)
+  y = y.toLowerCase();
+ // 4.)
+    var newCity = city.toLowerCase();
+ // 5.)
+     var animal = dog[1].toLowerCase();
+ // 6.)
+     alert((plant.toUpperCase));
+ // 7.)
+    var cityName = "kaRacHi";
+    var firstWord = cityName.slice(0,1).toUpperCase();
+    var remainWord = cityName.slice(1).toLowerCase();
+    var finalWord = firstWord + remainWord;
+    document.write(finalWord);
+
+           //   Chapter 21
+
+        //    1.)
+        var someWord = "captain"
+        var segment = someWord.slice(1,3);
+        //    2.)
+        length
+        //    3.)
+        var animal = "elephant";
+        var seg = animal.slice(2, 6);
+        //    4.)
+        var nums = prompt("enter number");
+        var lengthOfNums = (nums.length);
+        console.log(lengthOfNums);
+           5.)
+        var char = prompt("Enter character");
+        var i = char.length;
+        var seg = char.slice(1 , i-1);
+        var final = seg;
+        console.log(final);
+        //    6.)
+        var text = "To be or not to be.";
+        var indx = text.indexOf("be");
+          (3)
+        //    7.)
+          (16)
+        //    8.)
+        var text = "go";
+        var indx = text.lastIndexOf("go");
+        //    9.)
+        if (indexNum !== -1) 
+        //    10.)
+           (c)
+        //    11.)
+        var cha = text.charAt(9);
+        //    12.)
+        var x = str.charAt(str.length - 1);
+        //    13.)
+        var cha = input.charAt(4);
+        //    14.) 
+        if (x.charAt(2) === "y") 
+        //    15.)
+        for (var i = 0; i < text.length; i++)
+        //    16.)
+        var newStr = str.replace("1", "one");
+        //    17.)
+        (/__/g)
+            
+                      // Chapter 26 (Rounding Numbers)
+           
+        // 1.)
+        var roundedNum = math.round(num);
+        // 2.)
+        var roundedNum = math.celi(num);
+        // 3.)
+        var roundedNum = math.floor(num);
+        // 4.)
+        var roundNum = Math.ceil(num);
+        // 5.) 
+        var num = Math.floor(.5);
+
+                     // Chapter 27 (Random Numbers)
+        // 1.)
+        var num = math.random(50);
+        // 2.)
+        var randomNum = math.random();
+        // 3.)
+        var dice = (num * 6) + 1;
+        // 4.)     
+        var randomNumber = Math.random();
+        if (randomNumber < 0.5) {
+          console.log("Heads");
+        } else {
+          console.log("Tails");
+        }  
+            //  Chapter 28, 29 (Converting Strings)
+
+        // 1.)
+        var num = parsInt(prompt("Enter number"));
+           or
+        var num = +prompt("Enter number");   
+        // 2.)
+        var num = +prompt(123);
+        // 3.
+        var num = parseFloat("5.12")
+        // 4.)
+        var completNum = parseInt(x) + parseInt(x);
+        // 5.)
+        var num1 = Number(prompt("Enter first value"));
+        var num2 = Number(prompt("Enter second value"));
+        var total = num1 + num2;
+        console.log(typeof total.toString());
+       
+        // 6.)
+        var num = 42;
+        console.log(typeof num.toString);
+        // 7.)
+        var floatingNumString = "3.14";
+        var num = Number(floatingNumString);
+
+
+        //    Chapter 30 (Controlling the length of decimals)
+
+
+        // 1.)
+        var newNum = num.toFixed(4);
+        // 2.)
+        var newNum = num.toFixed(2);
+        num = Number(newNum);
+               or 
+        num = Number(num.toFixed(2));        
+        // 3.)
+        if (num.toFixed(2).length > 4){
+          console.log(num)
+        }
+        // 4.)
+        var num = 4.5542
+        alert(num.toFixed(2));
+
+                  // **********ASSINGMENT 5 JS COMPLETED***************
+           
+           
+          
+           
